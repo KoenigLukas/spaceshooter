a0525ab8dceb80c0c16ea7e554d1be69b55ab907
Benjamin Schellmann <43296230+SchellmannBenjamin@users.noreply.github.com>
2019-12-17 12:00:16 +0100

    Window scale

--- spaceshooter/core/src/com/gdx/game/Spaceshooter.java
+++ spaceshooter/core/src/com/gdx/game/Spaceshooter.java
@@ -3,23 +3,31 @@ package com.gdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Spaceshooter extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+
+	private OrthographicCamera camera;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 1280, 720);
+
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
