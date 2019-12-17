ca107def818ba24fb6c1b530ebdde955c0506462
Lukas König <lukas.koenig02@gmail.com>
2019-12-17 10:56:06 +0100

    bullet

--- spaceshooter/core/src/com/gdx/game/bullets/Bullet.java
+++ spaceshooter/core/src/com/gdx/game/bullets/Bullet.java
@@ -1,10 +1,21 @@
 package com.gdx.game.bullets;
 
-public abstract class Bullet {
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
+public abstract class Bullet extends Rectangle {
 
 
     private int damage;
     private float movSpeedFactor;
+    private Texture texture;
+
+    public Bullet(float x, float y, float width, float height, int damage, float movSpeedFactor,Texture texture) {
+        super(x, y, width, height);
+        this.damage = damage;
+        this.movSpeedFactor = movSpeedFactor;
+        this.texture = texture;
+    }
 
     public abstract void spawnBullet();
     public abstract void moveBullet();
