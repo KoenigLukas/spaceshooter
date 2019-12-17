66be359f084c196c0fd3b0fb828b5a6f36c0f710
Lukas König <lukas.koenig02@gmail.com>
2019-12-17 11:25:10 +0100

    added abstract classes

---
+++ spaceshooter/core/src/com/gdx/game/collectables/Collectable.java
@@ -0,0 +1,19 @@
+package com.gdx.game.collectables;
+
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
+public abstract class Collectable extends Rectangle {
+
+    private Texture texture;
+
+    public Collectable(float x, float y, float width, float height, Texture texture) {
+        super(x, y, width, height);
+        this.texture = texture;
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
+}
---
+++ spaceshooter/core/src/com/gdx/game/spaceships/SpaceShip.java
@@ -0,0 +1,33 @@
+package com.gdx.game.spaceships;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.gdx.game.bullets.Bullet;
+
+public abstract class SpaceShip extends Rectangle {
+
+    private float movSpeedFactor;
+    private float lifes;
+    private Texture texture;
+
+    public SpaceShip(float x, float y, float width, float height, float movSpeedFactor, float lifes, Texture texture) {
+        super(x, y, width, height);
+        this.movSpeedFactor = movSpeedFactor;
+        this.lifes = lifes;
+        this.texture = texture;
+    }
+
+    public abstract void shootBullet(Bullet bullet);
+
+    public float getMovSpeedFactor() {
+        return movSpeedFactor;
+    }
+
+    public float getLifes() {
+        return lifes;
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
+}
